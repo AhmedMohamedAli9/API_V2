d7a3bd01874bfe95d87018ea97eb21fe1d78f9e9
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-06 18:00:06 +0300

    This is a work in progress and I am lost af

--- src/test/java/com/myfatoorah/api/testcases/list/GetBankListTest.java
+++ src/test/java/com/myfatoorah/api/testcases/list/GetBankListTest.java
@@ -1,19 +1,34 @@
 package com.myfatoorah.api.testcases.list;
+
 import com.myfatoorah.api.apis.list.ListAPIs;
 import io.restassured.response.Response;
 import com.myfatoorah.api.models.Auth;
+
 import java.util.*;
+
 import org.testng.annotations.Test;
-import static io.restassured.RestAssured.given;
+
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
 public class GetBankListTest extends Auth {
 
     @Test
-    public void shouldReturnBanks(){
+    public void shouldReturnBanks() {
         Response response = ListAPIs.getBanks();
 
         assertThat(response.statusCode(), equalTo(200));
-        assertThat(response."Value", not(equalTo("")));
+        // Check if each bank has a 'Value' & 'Text' key and is not empty
+        List<Map<String, Object>> responseList = response.getBody().jsonPath().getList("");
+        for (Map<String, Object> item : responseList) {
+            assertThat(item, hasKey("Value"));
+            assertThat(item.get("Value").toString(), not(equalTo("")));
+            assertThat(item.get("Value").toString(), not(equalTo(null)));
+            // Optionally, check for the 'Text' key as well if needed
+            assertThat(item, hasKey("Text"));
+            assertThat(item.get("Text").toString(), not(equalTo("")));
+            assertThat(item.get("Text").toString(), not(equalTo(null)));
+        }
     }
 }
